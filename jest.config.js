59f6cb32ce2f0608fde044522acc240571015e13
zetavg <mail@zeta.vg>
2024-01-26 02:14:08 +0800

    disable type checking for running jest tests

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        // ts-jest options
+        isolatedModules: true, // Disables type-checking for running tests
+      },
+    ],
+  },
 };
